941a16553efc94f4c97c18f6409939205836845b
Adri <adriandiez1@gmail.com>
2023-10-26 09:34:34 +0200

    antes de la desgracia

---
+++ TareaiOS/gameController.swift
@@ -0,0 +1,13 @@
+//
+//  gameController.swift
+//  TareaiOS
+//
+//  Created by Adrian  on 25/10/23.
+//
+
+import UIKit
+
+class gameController: NSObject {
+
+
+}
---
+++ TareaiOS/viewGame.swift
@@ -0,0 +1,12 @@
+//
+//  viewGame.swift
+//  TareaiOS
+//
+//  Created by Adrian  on 25/10/23.
+//
+
+import UIKit
+
+class viewGame: NSObject {
+
+}
