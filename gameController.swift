0dc7f5ce95a92b11c4153ed5db2f9f264d73a3b4
adricev <adriandiez1@gmail.com>
2023-11-07 14:11:45 +0100

    Añadido logo de la app, simplificado el documento en el que se guardan las imágenes, ya cargan todas y se muestran, queda programar el funcionamiento el botón que hace el shuffle

--- gameController.swift
+++ gameController.swift
@@ -11,32 +11,30 @@ class gameController: UIViewController {
     
     @IBOutlet var imageViews: [UIImageView]!
     
-    
     // Nombres de las imágenes en tu proyecto
     let imageNames = ["PorscheAzul", "PorscheAmarillo", "PorscheRojo", "PorscheVerde", "PorscheBlanco", "PorscheNavy"]
     
-    @IBAction func jugarButtonTapped(_ sender: UIButton) {
-        // Baraja los nombres de las imágenes aleatoriamente
-        let shuffledImageNames = imageNames.shuffled()
-        
-        for (index, imageView) in imageViews.enumerated() {
-            if index < shuffledImageNames.count {
-                // Asigna una imagen aleatoria a cada ImageView
-                if let image = UIImage(named: shuffledImageNames[index]) {
-                    imageView.image = image
-                }
-            }
-        }
-    }
+    var shuffledImageNames: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Baraja los nombres de las imágenes aleatoriamente
+        shuffledImageNames = imageNames.shuffled()
         // Inicialmente, asigna imágenes aleatorias
         assignRandomImages()
     }
     
+    @IBAction func jugarButtonTapped(_ sender: UIButton) {
+        // Baraja nuevamente las imágenes para mostrar diferentes combinaciones
+        shuffledImageNames = imageNames.shuffled()
+        assignRandomImages()
+    }
+    
     func assignRandomImages() {
-        let shuffledImageNames = imageNames.shuffled()
+        guard imageViews.count >= shuffledImageNames.count else {
+            // Manejar el caso en el que haya menos ImageViews que imágenes disponibles
+            return
+        }
         
         for (index, imageView) in imageViews.enumerated() {
             if index < shuffledImageNames.count {
